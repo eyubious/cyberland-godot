201dab40518d2c5000b94033600ac73de073ee01
Mariana Perez <mperezh@email.unc.edu>
2023-10-14 14:27:24 -0400

    First commit: Interface and navigation

---
+++ main_menu.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_map_btn_pressed():
+	get_tree().change_scene_to_file("res://cyberland_map.tscn")
+
+func _on_collections_btn_pressed():
+	get_tree().change_scene_to_file("res://collections.tscn")
+
+func _on_shop_btn_pressed():
+	get_tree().change_scene_to_file("")
+
+func _on_questions_btn_pressed():
+	get_tree().change_scene_to_file("")
+
+
+func _on_avatar_btn_pressed():
+	get_tree().change_scene_to_file("")
+
+
+func _on_files_btn_pressed():
+	get_tree().change_scene_to_file("res://files.tscn")
