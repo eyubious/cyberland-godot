119b70bc4a195a337f9118543192d3df27d516d2
William Garcia-Sanchez <89278772+Wixx19@users.noreply.github.com>
2023-10-31 15:49:14 -0400

    video plays after 3 seconds

--- Missions/mission_1/task1.gd
+++ Missions/mission_1/task1.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	start_video_with_delay()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,11 +15,15 @@ func _process(delta):
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://cyberland_map.tscn")
 
+#func _on_button_2_pressed():
+	#var video_player = $VideoStreamPlayer
+	#if video_player.is_playing():
+	#	video_player.stop()  # Pause the video if it's currently playing
+	#else:
+	#	video_player.play()  # Resume the video if it's currently paused
 
-func _on_button_2_pressed():
-	var video_player = $VideoStreamPlayer # Assuming the VideoPlayer node is named "VideoStreamPlayer"
-	
-	if video_player.is_playing():
-		video_player.stop()  # Pause the video if it's currently playing
-	else:
-		video_player.play()  # Unpause the video if it's currently paused
+
+func start_video_with_delay():
+	var video_player = $VideoStreamPlayer
+	await get_tree().create_timer(3).timeout # Wait for 3 seconds
+	video_player.play()  # Play the video after the delay
