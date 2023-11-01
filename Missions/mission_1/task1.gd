ff9a81b47c52ccbebadaebc32cc2edf607b3dd57
William Garcia-Sanchez <89278772+Wixx19@users.noreply.github.com>
2023-11-01 13:29:48 -0400

    Added background and try other things.
    Tried doing a function that disables button until video is finished. Video playback is delayed 2 seconds. Basic background and button. Everything subject to change.

--- Missions/mission_1/task1.gd
+++ Missions/mission_1/task1.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+var video_player : VideoStreamPlayer
+var continue_button : Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	continue_button = $ContinueButton
+	
 	start_video_with_delay()
-	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,15 +18,19 @@ func _process(delta):
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://cyberland_map.tscn")
 
+#code if you want play/stop but if you resume video will start all over.
 #func _on_button_2_pressed():
 	#var video_player = $VideoStreamPlayer
 	#if video_player.is_playing():
 	#	video_player.stop()  # Pause the video if it's currently playing
 	#else:
 	#	video_player.play()  # Resume the video if it's currently paused
+	
+#TODO find a way to make button appear after video is done 
 
 
 func start_video_with_delay():
 	var video_player = $VideoStreamPlayer
-	await get_tree().create_timer(3).timeout # Wait for 3 seconds
+	await get_tree().create_timer(2).timeout # Wait for 3 seconds
 	video_player.play()  # Play the video after the delay
+
