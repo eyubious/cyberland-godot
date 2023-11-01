8e677a4f46dcbf2263f3623356c42e038d1a1443
Mariana Perez <mperezh@email.unc.edu>
2023-11-01 14:20:54 -0400

    modified names

--- Missions/mission_1/task1.gd
+++ Missions/mission_1/task1.gd
@@ -16,7 +16,7 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://cyberland_map.tscn")
+	get_tree().change_scene_to_file("")
 
 #code if you want play/stop but if you resume video will start all over.
 #func _on_button_2_pressed():
