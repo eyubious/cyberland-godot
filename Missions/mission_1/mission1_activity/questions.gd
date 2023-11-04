687f72fb55600605b778c96c0c75bb77791e9998
Mariana Perez <mperezh@email.unc.edu>
2023-11-04 01:34:11 -0400

    adding questions in json

--- Missions/mission_1/mission1_activity/questions.gd
+++ Missions/mission_1/mission1_activity/questions.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+# question variables
+var displayQuestion = $question
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +11,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta) -> void:
 	pass
+	
+func 
