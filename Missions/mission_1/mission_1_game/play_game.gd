8fd0c36a7e3dadcec3e422599434d34ba7950308
Mariana Perez <mperezh@email.unc.edu>
2023-11-08 12:01:25 -0500

    added timer

--- Missions/mission_1/mission_1_game/play_game.gd
+++ Missions/mission_1/mission_1_game/play_game.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+# timer variables
+var time: float = 10.0
+var mins: int = 0
+var secs: int = 0
+var msec: int = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,7 +14,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	time -= delta
+	msec = fmod(time, 1) * 100
+	secs = fmod(time, 60)
+	mins = fmod(time, 3600) / 60
+	$timer/mins.text = "%02d:" % mins
+	$timer/secs.text = "%02d." % secs
+	$timer/msec.text = "%03d" % msec
+	
+	if (mins == 0 && secs == 0 && msec == 0):
+		stop()
+		
+func stop() -> void:
+	set_process(false)
+	
+func format_time() -> String:
+	return "%02d:%02d.%03d" % [mins, secs, msec]
 
 
 func _on_game_over_btn_pressed():
