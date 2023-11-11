a36c480424b7ea8991e8d26446b2324385f7f8e0
Mariana Perez <mperezh@email.unc.edu>
2023-11-11 17:10:58 -0500

    added questions and answers

--- Missions/mission_1/mission_1_game/game_end.gd
+++ Missions/mission_1/mission_1_game/game_end.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var message = $endDescription
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,7 +10,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	display_score()
+	
+func display_score():
+	var score # = round(correct / dict.size() * 100)
+	var response
+	if score >= 21:
+		response = "You answered all the questions correctly. Congratulations!"
+	elif score <= 20 and score >= 18:
+		response = "Congratulations, you missed a few questions, but you've learned many new things!"
+	else:
+		response = "It looks like you have some shortcomings. The good news is that you've learned a lot. Continue to learn!"
 
 
 func _on_next_btn_pressed():
--- Missions/mission_1/mission_1_game/play_game.gd
+++ Missions/mission_1/mission_1_game/play_game.gd
@@ -6,11 +6,27 @@ var mins: int = 0
 var secs: int = 0
 var msec: int = 0
 
+# load data to dictionary
+var dict = read_file("res://Missions/mission_1/mission_1_game/question_content.json")
+var item: Dictionary
+var item_index: int = 0
+
+# question & answers variables
+@onready var displayQuestion = $VBoxContainer/question
+@onready var displayAnswerChoices = $VBoxContainer/answerChoices
+var correct: float = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	refresh_screen()
+	
+	
+func refresh_screen():
+	if item_index >= dict.size():
+		show_results()
+	else:
+		show_question()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -18,9 +34,9 @@ func _process(delta):
 	msec = fmod(time, 1) * 100
 	secs = fmod(time, 60)
 	mins = fmod(time, 3600) / 60
-	$timer/mins.text = "%02d:" % mins
-	$timer/secs.text = "%02d." % secs
-	$timer/msec.text = "%03d" % msec
+	$VBoxContainer/timer/mins.text = "%02d:" % mins
+	$VBoxContainer/timer/secs.text = "%02d." % secs
+	$VBoxContainer/timer/msec.text = "%03d" % msec
 	
 	if (mins == 0 && secs == 0 && msec == 0):
 		stop()
@@ -30,7 +46,30 @@ func stop() -> void:
 	
 func format_time() -> String:
 	return "%02d:%02d.%03d" % [mins, secs, msec]
+	
+func show_question():
+	displayQuestion.show()
+	displayAnswerChoices.show()
+	displayAnswerChoices.clear()
+	item = dict[item_index]
+	displayQuestion.text = item.question
+	var answers = item.answers
+	for answer in answers:
+		displayAnswerChoices.add_item(answer)
+
+func show_results():
+	get_tree().change_scene_to_file("res://Missions/mission_1/mission1_game/game_end.tscn")
+	
+	
+func read_file(file):
+	var json_as_text = FileAccess.get_file_as_string(file)
+	var json_as_dict = JSON.parse_string(json_as_text)
+	return json_as_dict
+	
 
 
-func _on_game_over_btn_pressed():
-	get_tree().change_scene_to_file("res://Missions/mission_1/mission_1_game/game_end.tscn")
+func _on_answer_choices_item_selected(index):
+	if index == item.i_correct:
+		correct += 1
+	item_index += 1
+	refresh_screen()
