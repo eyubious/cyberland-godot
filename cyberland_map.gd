a7565ba5cba5117efd4c9807ed3a0349ed434bad
Mariana Perez <mperezh@email.unc.edu>
2023-11-13 16:46:32 -0500

    organization

--- cyberland_map.gd
+++ cyberland_map.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
+# button variables
+@onready var mission1Btn = $Task1_Button
+@onready var mission2Btn = $Task2_Button
+@onready var mission3Btn = $Task3_Button
+@onready var mission4Btn = $Task4_Button
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
