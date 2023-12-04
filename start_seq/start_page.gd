bf9f3f2cec62c73943493e0d8ccca276645ef16f
Kaival Patel <69801409+kaival1524@users.noreply.github.com>
2023-12-04 15:35:23 -0500

    have some basic testing. still issues on gut testing.

--- start_seq/start_page.gd
+++ start_seq/start_page.gd
@@ -1,15 +1,27 @@
-extends Node2D
+extends GutTest
+func before_each():
+	gut.p("ran setup", 2)
 
+func after_each():
+	gut.p("ran teardown", 2)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func before_all():
+	gut.p("ran run setup", 2)
 
+func after_all():
+	gut.p("ran run teardown", 2)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func test_assert_eq_number_not_equal():
+	assert_eq(1, 2, "Should fail.  1 != 2")
 
-func _on_start_btn_pressed():
-	get_tree().change_scene_to_file("res://main_menu.tscn")
+func test_assert_eq_number_equal():
+	assert_eq('asdf', 'asdf', "Should pass")
 
+func test_assert_true_with_true():
+	assert_true(true, "Should pass, true is true")
+
+func test_assert_true_with_false():
+	assert_true(false, "Should fail")
+
+func test_something_else():
+	assert_true(false, "didn't work")
