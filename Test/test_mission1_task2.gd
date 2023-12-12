e313d677dd337af02a44cc2b6613565454e706ef
Mariana Perez <mperezh@email.unc.edu>
2023-12-12 09:46:02 -0500

    testing outline

--- Test/test_mission1_task2.gd
+++ Test/test_mission1_task2.gd
@@ -1,4 +1,31 @@
 extends "res://addons/gut/test.gd"
 
-var gameScene = preload("res://Missions/mission_1/mission_1_game/play_game.gd")
+var gameScene = preload("res://Missions/mission_1/mission_1_game/play_game.tscn")
+var gameScript = load("res://Missions/mission_1/mission_1_game/play_game.gd")
+var score = load("res://Missions/mission_1/mission_1_game/score.gd")
 
+var test_data = {
+		"question" : "What is cyberbullying?",
+		"image" : "res://Missions/mission_1/mission_1_game/art/1.jpg",
+		"answers" : [
+			"Harrassment or bullying that occurs through electronic devices",
+			"Physical bullying towards the person",
+			"Bullying that occurs only verbally"
+			],
+		"i_correct" : 0
+	}
+
+# func test_point_addition():
+	
+	
+# func test_point_subtraction():
+	
+	
+# func test_answer_item_click():
+	
+	
+# func test_answer_correct():
+	
+	
+# fuct test_answer_incorrect():
+	
