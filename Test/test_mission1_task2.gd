c0796d007af08c73d1bec0b6f39cae83e8a651e6
William Garcia-Sanchez <89278772+Wixx19@users.noreply.github.com>
2023-12-11 21:01:59 -0500

    unit test

--- Test/test_mission1_task2.gd
+++ Test/test_mission1_task2.gd
@@ -13,3 +13,29 @@ func test_assert_true_with_true():
 
 func test_assert_true_with_false():
 	assert_true(false, "Should fail")
+	
+	
+func test_run():
+	# Instantiate the scene
+	var button_scene = load("res://start_seq/start_page.tscn").instantiate()
+
+	# Add the instance to the scene
+	add_child(button_scene)
+
+	# Connect the signal from the button
+	button_scene.connect("_on_start_btn_pressed", self, "_on_button_test_pressed".to_int())
+
+	# Simulate button press
+	button_scene.get_node("Button").emit_signal("_on_start_btn_pressed")
+
+
+func _on_button_test_pressed():
+	# Assert the expected behavior after the button press
+	assert(true, "Button press test passed!")
+
+
+
+
+
+
+
