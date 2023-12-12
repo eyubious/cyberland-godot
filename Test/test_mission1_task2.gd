30babed2b64c9a3e3bc6c3b728371db434d82416
Mariana Perez <mperezh@email.unc.edu>
2023-12-12 09:03:38 -0500

    adding branch

--- Test/test_mission1_task2.gd
+++ Test/test_mission1_task2.gd
@@ -2,14 +2,3 @@ extends "res://addons/gut/test.gd"
 
 var gameScene = preload("res://Missions/mission_1/mission_1_game/play_game.gd")
 
-func test_assert_eq_number_not_equal():
-	assert_eq(1, 2, "Should fail.  1 != 2")
-
-func test_assert_eq_number_equal():
-	assert_eq('asdf', 'asdf', "Should pass")
-
-func test_assert_true_with_true():
-	assert_true(true, "Should pass, true is true")
-
-func test_assert_true_with_false():
-	assert_true(false, "Should fail")
